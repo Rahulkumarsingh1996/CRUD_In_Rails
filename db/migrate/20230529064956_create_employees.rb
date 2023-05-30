3993845940ad2ca84138931472590afb203ce3cb
rahulkumarsingh1996~ <singhrahul841412@gmail.com>
2023-05-30 11:27:42 +0530

    first

---
+++ db/migrate/20230529064956_create_employees.rb
@@ -0,0 +1,13 @@
+class CreateEmployees < ActiveRecord::Migration[7.0]
+  def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :linkedln
+
+      t.timestamps
+    end
+  end
+end
